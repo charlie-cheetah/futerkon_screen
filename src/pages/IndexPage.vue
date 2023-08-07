e69f86a3f46d54e0969ec174f528b08e07870be8
Krzysztof Bedra <kbedra@tbscg.com>
2023-08-08 00:25:04 +0200

    update

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,15 +2,15 @@
   <q-page class="flex bg-blue-grey-14" :style-fn="styleTweak">
     <div class="row max-inherit full-width">
       <div class="col-8 max-inherit column">
-        <q-btn v-if="agenda?.current?.length" text-color="black"  color="green-2" size="lg" class="col-auto q-ma-xs">Aktualnie trwa:</q-btn>
+        <q-btn v-if="agenda?.current?.length" text-color="black"  color="indigo-2" size="lg" class="col-auto q-ma-xs">Aktualnie trwa:</q-btn>
         <div class="overflow-auto col" ref="scrollable">
           <div class="q-gutter-md q-pa-md">
             <q-card v-for="a in agenda.current" :key="a.agenda_id" class="current-card" ref="currentCard">
-              <q-card-section class="bg-green-2 q-py-sm">
-                <div class="text-h3 q-mt-sm q-mb-xs">{{ a.name }}
+              <q-card-section class="bg-indigo-2 q-py-sm">
+                <div class="text-h3 q-mt-sm q-mb-xs font-weigh-bold">{{ a.name }}
                   <q-chip dark size="lg" v-if="a.settings.canceled && a.settings.canceled === 'on'" color="negative" class="q-ml-sm">odwołany!</q-chip>
                   <q-chip dark size="lg" v-if="a.settings.changed && a.settings.changed === 'on'" color="orange-10" class="q-ml-sm">zmiana!</q-chip>
-                  <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">konkurs</q-chip>
+                  <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">punktowany</q-chip>
                 </div>
               </q-card-section>
               <q-card-section horizontal>
@@ -31,7 +31,7 @@
                   />
                 </div>
               </q-card-section>
-              <q-linear-progress stripe rounded size="20px" :value="a.progress/100" color="green-4" />
+              <q-linear-progress stripe rounded size="20px" :value="a.progress/100" color="indigo-4" />
             </q-card>
           </div>
         </div>
@@ -42,10 +42,10 @@
           <div class="q-gutter-md q-pa-md">
             <q-card v-for="a in agenda.upcomming" :key="a.agenda_id">
               <q-card-section class="bg-orange-2 q-py-sm">
-                <div class="text-h3 q-mt-sm q-mb-xs">{{ a.name }}
+                <div class="text-h3 q-mt-sm q-mb-xs font-weigh-bold">{{ a.name }}
                   <q-chip dark size="lg" v-if="a.settings.canceled && a.settings.canceled === 'on'" color="negative" class="q-ml-sm">odwołany!</q-chip>
                   <q-chip dark size="lg" v-if="a.settings.changed && a.settings.changed === 'on'" color="orange-10" class="q-ml-sm">zmiana!</q-chip>
-                  <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">konkurs</q-chip>
+                  <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">punktowany</q-chip>
                 </div>
               </q-card-section>
 
@@ -75,7 +75,7 @@ export default defineComponent({
     const agenda = ref({});
     const currentCard = ref(null)
     const dismiss = ref(null)
-    const nowDisplaying = ref(3)
+    const nowDisplaying = ref(0)
     const $q = useQuasar()
 
     const styleTweak = (offset) =>
@@ -83,7 +83,7 @@ export default defineComponent({
       return {maxHeight: offset ? `calc(100vh - ${offset}px)` : '100vh', height: offset ? `calc(100vh - ${offset}px)` : '100vh'}
     }
     const timer = function () {
-      fetch('https://2023.futerkon.pl/wp-json/wpvue/agenda_screen')
+      fetch('https://2023.futerkon.pl/wp-json/wpvue/agenda_screen' + window.location.search)
         .then(response => { console.log('tick!'); return response.json();})
         .then(data => agenda.value = data)
     }
@@ -107,12 +107,14 @@ export default defineComponent({
     );
 
     const scroll = function() {
-      if (nowDisplaying.value <= currentCard.value.length) {
-        currentCard.value?.[nowDisplaying.value]?.$el.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
-        nowDisplaying.value = nowDisplaying.value + 1
+      if (nowDisplaying.value === 0) {
+        nowDisplaying.value = currentCard.value.length - 1
       } else {
-        nowDisplaying.value = 3;
+        nowDisplaying.value = 0;
       }
+      console.log(nowDisplaying.value);
+      currentCard.value?.[nowDisplaying.value]?.$el.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
+
     }
 
     setInterval(timer(),  10 * 1000) //co minute
