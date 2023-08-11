44eff768bb4c4cb20138034499add24ad9b5afbc
Krzysztof Bedra <kbedra@tbscg.com>
2023-08-11 11:25:08 +0200

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,27 +1,23 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar class="bg-blue-grey-10">
-        <img src="./../../public/icons/apple-touch-icon.png" height="48">
-        <q-toolbar-title class="text-h4">
-          Agenda
-        </q-toolbar-title>
-
-        <div class="text-h4">
-          <span class="text-h6 q-mr-lg">{{ date }}</span>
-          <span class="text-h4">{{ time }}</span>
-        </div>
-      </q-toolbar>
-    </q-header>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+  <div class="bg-blue-grey-14 overscan full-height">
+    <q-toolbar class="bg-blue-grey-10 text-white">
+      <img src="./../../public/icons/apple-touch-icon.png" height="48">
+      <q-toolbar-title class="text-h4">
+        Agenda {{ screenLocation ? ' - ' + screenLocation.name : '' }}
+      </q-toolbar-title>
+      <q-btn round flat @click="setOverscan(true)">+</q-btn>
+      <q-btn round flat class="q-mr-md" @click="setOverscan(false)">-</q-btn>
+      <div class="text-h4">
+        <span class="text-h6 q-mr-lg">{{ date }}</span>
+        <span class="text-h4">{{ time }}</span>
+      </div>
+    </q-toolbar>
+    <router-view :overscan="overscan" @location="saveLocation"/>
+  </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
@@ -30,6 +26,25 @@ export default defineComponent({
     const options = { weekday: 'long', month: 'long', day: 'numeric' }
     const date = ref(now.toLocaleDateString('pl-PL', options))
     const time = ref(now.toLocaleTimeString('pl-PL'))
+    const overscan = ref(Number(localStorage.getItem("overscan")))
+
+    const setOverscan = (more) => {
+      if (more) {
+        overscan.value = overscan.value + 5
+      } else {
+        overscan.value = overscan.value - 5
+      }
+      localStorage.setItem("overscan", overscan.value)
+    }
+
+    const overscanInPx = computed(() => overscan.value + 'px')
+
+    const screenLocation = ref(null)
+
+    const saveLocation = (loc) => {
+      screenLocation.value = loc
+    }
+
     setInterval(()=>{
       const now = new Date()
       date.value = now.toLocaleDateString('pl-PL', options)
@@ -37,8 +52,18 @@ export default defineComponent({
     }, 1000)
     return {
       date,
-      time
+      time,
+      saveLocation,
+      setOverscan,
+      screenLocation,
+      overscan,
+      overscanInPx
     }
   }
 })
 </script>
+<style scoped>
+.overscan {
+  padding: v-bind('overscanInPx');
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,59 +1,60 @@
 <template>
-  <q-page class="flex bg-blue-grey-14" :style-fn="styleTweak">
-    <div class="row max-inherit full-width">
+    <div class="row bg-blue-grey-14 max-height-calc full-width" :style="{maxHeight: `calc(100vh - ${overscan * 2}px - 50px)`, height: `calc(100vh - ${overscan * 2}px - 50px)`}">
       <div class="col-8 max-inherit column">
-        <q-btn v-if="agenda?.current?.length" text-color="black"  color="indigo-2" size="lg" class="col-auto q-ma-xs">Aktualnie trwa:</q-btn>
+        <q-btn v-if="agenda?.current?.length" text-color="black" color="indigo-2" size="lg" class="col-auto q-ma-xs">Aktualnie trwa:</q-btn>
         <div class="overflow-auto col" ref="scrollable">
           <div class="q-gutter-md q-pa-md">
             <q-card v-for="a in agenda.current" :key="a.agenda_id" class="current-card" ref="currentCard">
-              <q-card-section class="bg-indigo-2 q-py-sm">
-                <div class="text-h3 q-mt-sm q-mb-xs font-weigh-bold">{{ a.name }}
-                  <q-chip dark size="lg" v-if="a.settings.canceled && a.settings.canceled === 'on'" color="negative" class="q-ml-sm">odwołany!</q-chip>
-                  <q-chip dark size="lg" v-if="a.settings.changed && a.settings.changed === 'on'" color="orange-10" class="q-ml-sm">zmiana!</q-chip>
-                  <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">punktowany</q-chip>
-                </div>
-              </q-card-section>
               <q-card-section horizontal>
-                <q-card-section class="preview q-pt-sm q-pb-none" :class="a.image.full?'col-10':'col-12'">
-                  <div class="row">
-                    <div class="col-6 self-center flex items-center"><span class="text-weight-bold text-h4">{{ formatTime(a) }}</span><q-icon size="sm" name="schedule" class="q-ml-md" /><span class="text-h5 q-ml-xs">{{ formatDuration(a) }}</span></div>
-                    <div class="col-6  text-weight-bold text-right">
-                      <q-btn color="primary" size="lg" icon="location_on" :label="a.location.name" />
+                <q-card-section class="q-pa-none">
+                  <q-card-section class="q-py-sm rounded-borders-tr" :class="screenLocation?.id === a.location.id ? 'bg-indigo-10' : 'bg-indigo-2'">
+                    <div class="text-h3 q-mt-sm q-mb-xs font-weigh-bold" :class="{'text-white':screenLocation?.id === a.location.id}">{{ a.name }}
+                      <q-chip dark size="lg" v-if="a.settings.canceled && a.settings.canceled === 'on'" color="negative" class="q-ml-sm">odwołany!</q-chip>
+                      <q-chip dark size="lg" v-if="a.settings.changed && a.settings.changed === 'on'" color="orange-10" class="q-ml-sm">zmiana!</q-chip>
+                      <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">punktowany</q-chip>
                     </div>
-                  </div>
-                  <div class="q-mt-md truncate-overflow text-h6 text-weight-regular" v-html="a.post_content" />
+                  </q-card-section>
+                  <q-card-section class="preview q-py-sm" :class="[a.image.full?'col-10':'col-12',{'bg-indigo-7 text-white':screenLocation?.id === a.location.id}]" >
+                    <div class="row">
+                      <div class="col-6 self-center flex items-center"><span class="text-weight-bold text-h4">{{ formatTime(a) }}</span><q-icon size="sm" name="schedule" class="q-ml-md" /><span class="text-h5 q-ml-xs">{{ formatDuration(a) }}</span></div>
+                      <div class="col-6  text-weight-bold text-right">
+                        <q-btn color="primary" size="lg" :icon=" screenLocation?.id === a.location.id ? 'my_location' : 'location_on'" :label="screenLocation?.id === a.location.id ? 'tutaj' : a.location.name" @click="saveLocation(a.location)" />
+                      </div>
+                    </div>
+                    <div class="q-mt-sm truncate-overflow text-h6 text-weight-regular" v-html="a.post_content" />
+                  </q-card-section>
+                  <q-linear-progress stripe class="rounded-borders-br" size="20px" :value="a.progress/100" :color="screenLocation?.id === a.location.id ? 'indigo-10' : 'indigo-4'" />
                 </q-card-section>
-                <div v-if="a.image.full" class="col-2 items-end">
+                <div class="ima col-2">
                   <q-img
-                    class="preview"
                     :src="a.image.full"
-                    fit="contain"
+                    fit="cover"
+                    class="full-height rounded-borders-l"
                   />
                 </div>
               </q-card-section>
-              <q-linear-progress stripe rounded size="20px" :value="a.progress/100" color="indigo-4" />
             </q-card>
           </div>
         </div>
       </div>
       <div class="col-4 max-inherit column">
-        <q-btn v-if="agenda?.upcomming?.length" text-color="black" color="orange-2" size="lg" class="col-auto q-ma-xs">Wkrótce będzie:</q-btn>
+        <q-btn v-if="agenda?.upcomming?.length" text-color="black" color="green-2" size="lg" class="col-auto q-ma-xs">Wkrótce będzie:</q-btn>
         <div class="overflow-hidden col">
           <div class="q-gutter-md q-pa-md">
             <q-card v-for="a in agenda.upcomming" :key="a.agenda_id">
-              <q-card-section class="bg-orange-2 q-py-sm">
-                <div class="text-h3 q-mt-sm q-mb-xs font-weigh-bold">{{ a.name }}
+              <q-card-section class="q-py-sm" :class="screenLocation?.id === a.location.id ? 'bg-green-10' : 'bg-green-2'">
+                <div class="text-h3 q-mt-sm q-mb-xs font-weigh-bold" :class="{'text-white':screenLocation?.id === a.location.id}">{{ a.name }}
                   <q-chip dark size="lg" v-if="a.settings.canceled && a.settings.canceled === 'on'" color="negative" class="q-ml-sm">odwołany!</q-chip>
                   <q-chip dark size="lg" v-if="a.settings.changed && a.settings.changed === 'on'" color="orange-10" class="q-ml-sm">zmiana!</q-chip>
                   <q-chip dark size="lg" v-if="a.settings.extra_points && a.settings.extra_points === 'on'" color="blue-10" class="q-ml-sm">punktowany</q-chip>
                 </div>
               </q-card-section>
 
-              <q-card-section class="preview q-pt-sm q-pb-sm">
+              <q-card-section class="preview q-pt-sm q-pb-sm" :class="{'bg-green-7 text-white':screenLocation?.id === a.location.id}">
                 <div class="row">
                   <div class="col-6 self-center"><span class="text-weight-bold text-h4">{{ formatTime(a) }}</span></div>
                   <div class="col-6  text-weight-bold text-right">
-                    <q-btn color="primary" size="lg" icon="location_on" :label="a.location.name" />
+                    <q-btn color="primary" size="lg" :icon=" screenLocation?.id === a.location.id ? 'my_location' : 'location_on'" :label="screenLocation?.id === a.location.id ? 'tutaj' : a.location.name" @click="saveLocation(a.location)" />
                   </div>
                 </div>
               </q-card-section>
@@ -62,7 +63,6 @@
         </div>
       </div>
     </div>
-  </q-page>
 </template>
 
 <script>
@@ -71,21 +71,25 @@ import { useQuasar } from 'quasar'
 
 export default defineComponent({
   name: 'IndexPage',
-  setup () {
+  emits: ['location'],
+  props: ['overscan'],
+  setup (props, { emit }) {
     const agenda = ref({});
     const currentCard = ref(null)
     const dismiss = ref(null)
     const nowDisplaying = ref(0)
     const $q = useQuasar()
+    const screenLocation = ref(null)
 
-    const styleTweak = (offset) =>
-    {
-      return {maxHeight: offset ? `calc(100vh - ${offset}px)` : '100vh', height: offset ? `calc(100vh - ${offset}px)` : '100vh'}
-    }
+    try {
+      screenLocation.value = JSON.parse(localStorage.getItem("location"))
+      emit('location', screenLocation.value)
+    } catch (e) {}
+
     const timer = function () {
       fetch('https://2023.futerkon.pl/wp-json/wpvue/agenda_screen' + window.location.search)
         .then(response => { console.log('tick!'); return response.json();})
-        .then(data => agenda.value = data)
+        .then(data => agenda.value = {current: data.current.sort((a) => a.location.id === screenLocation.value?.id ? -1 : 0), upcomming:data.upcomming})
     }
 
     window.addEventListener("offline",
@@ -112,7 +116,6 @@ export default defineComponent({
       } else {
         nowDisplaying.value = 0;
       }
-      console.log(nowDisplaying.value);
       currentCard.value?.[nowDisplaying.value]?.$el.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
 
     }
@@ -125,6 +128,16 @@ export default defineComponent({
       const start = new Date(a.start_date * 1000)
       const end = new Date((Number(a.start_date) + (a.duration * 60))*1000)
       return start.toLocaleTimeString('pl-PL', { hour: "2-digit", minute: "2-digit" }) + ' - ' + end.toLocaleTimeString('pl-PL', { hour: "2-digit", minute: "2-digit" })
+    }
+
+    const saveLocation = (location) => {
+      if (screenLocation.value?.id === location.id) {
+        screenLocation.value = null
+      } else {
+        screenLocation.value = location
+      }
+      emit('location', screenLocation.value)
+      localStorage.setItem("location", JSON.stringify(screenLocation.value))
     }
 
     const formatDuration = (a) =>{
@@ -145,10 +158,11 @@ export default defineComponent({
     }
     return {
       agenda,
-      styleTweak,
       formatTime,
       formatDuration,
-      currentCard
+      currentCard,
+      saveLocation,
+      screenLocation
     }
   }
 })
@@ -156,11 +170,22 @@ export default defineComponent({
 <style lang="scss">
 .max-inherit {
   max-height: inherit;
+  height: inherit;
 }
+
 .current-card {
-  .preview{
-    max-height: 9rem;
+  .rounded-borders-l {
+    border-radius: 0 4px 4px 0;
   }
+
+  .rounded-borders-br {
+    border-radius: 0 0 0 4px;
+  }
+
+  .rounded-borders-tr {
+    border-radius: 4px 0 0 0;
+  }
+
   .truncate-overflow {
     height: 4rem;
     overflow: hidden;
